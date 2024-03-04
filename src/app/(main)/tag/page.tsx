a79e286ecc025f9795869493293bd2381d2c4be7
Kristina Karpenko <karpenko.krista@gmail.com>
2024-03-04 20:59:06 +0100

    fix: build

--- src/app/(main)/tag/page.tsx
+++ src/app/(main)/tag/page.tsx
@@ -1,18 +1,18 @@
 import { Button, Stack, Typo, Wrapper } from '@/components';
-import { getTagList, tagToUrl } from './utils';
+// import { getTagList, tagToUrl } from './utils';
 
 const AllTagsPage = async () => {
-  const tags = await getTagList();
+  // const tags = await getTagList(); 
   return (
     <Wrapper tag="section">
       <Typo variant="header1">All Tags</Typo>
-      <Stack direction="row">
+      {/* <Stack direction="row">
         {tags.map((tag) => (
           <Button key={tag} href={tagToUrl(tag)} variant="text">
             {tag}
           </Button>
         ))}
-      </Stack>
+      </Stack> */}
     </Wrapper>
   );
 };
